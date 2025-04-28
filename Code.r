277b7f8cb2b2ebe26438dab0f9671394f23d69b9
jamev <jame.vallette@ensae.fr>
2025-04-28 13:05:09 +0000

    Progression lente mais sûre, test rapide de stationnarité de la série différentié : OK

--- Code.r
+++ Code.r
@@ -1,26 +1,47 @@
 #Projet de séries temporelles
 #Etude de la production de pâtes alimentaires
 
-datafile <- "pates_alim.csv"
-data <- read.csv(datafile,sep=";", skip=3, header = TRUE)
-
-
 install.packages("zoo")
 install.packages("tseries")
 
 require(zoo)
 require(tseries)
 
+#   QUESTION 2
 
-colnames(data) <- c("Periode", "Indice", "Code")
+##Importation et nettoyage rapide de la série
+datafile <- "pates_alim.csv"
+data <- read.csv(datafile,sep=";", skip=3, header = TRUE)
 
-# Conversion de la période en Date (on ajoute "-01" pour avoir un jour du mois)
-data$Date <- as.Date(paste0(data$Periode, "-01"))
+colnames(data) <- c("Periode", "Indice","Code")
+data$Periode <- as.Date(paste0(data$Periode, "-01"))
+xm_source <- zoo(data$Indice, order.by = data$Periode)
+T <-length(xm_source)
+xm <- xm_source[1:(T-4)]
 
-# Création du zoo
-xm_source <- zoo(data$Indice, order.by = data$Date)
+#Tracé de la série 
+plot(xm, type = "l", xlab = "Date", ylab = "Indice")
 
-# Tracé
-plot(xm_source, type = "l", xlab = "Date", ylab = "Indice", main = "Indice de Production Industrielle - Pâtes alimentaires")
+acf(coredata(xm))
+pacf(coredata(xm))
 
-head(data)
+#Première différenciation 
+xm_diff <- diff(xm)
+
+plot(xm_diff, type = "l", xlab = "Date", ylab = "Indice")
+acf(coredata(xm_diff))
+pacf(coredata(xm_diff))
+
+
+#   QUESTION 3
+
+## Ouverture d'une fenêtre à 2 graphiques
+par(mfrow = c(2, 1))  # 2 lignes, 1 colonne
+
+# Premier graphique : série log-transformée
+plot(xm, type = "l", col = "blue", xlab = "Date", ylab = "Indice")
+
+# Deuxième graphique : série différenciée
+plot(xm_diff, type = "l", col = "red", xlab = "Date", ylab = "Indice différencié")
+
+adf.test(coredata(xm_diff), alternative = "stationary")
