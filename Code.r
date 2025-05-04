a8a14738c6d4b1d3c76a8661bf71ca0497ed59e8
hmorand <hugo.morand@ensae.fr>
2025-05-04 16:43:54 +0000

    la serie boisson est pas mal

--- Code.r
+++ Code.r
@@ -1,39 +1,35 @@
 #Projet de séries temporelles
-#Etude de la production de pâtes alimentaires
+#Etude de la fabrication de boissons
 
+#Installation des packages necessaires
 install.packages("zoo")
 install.packages("tseries")
 
 require(zoo)
 require(tseries)
 
-#   QUESTION 2
+## PARTIE 1 ##
 
-##Importation et nettoyage rapide de la série
-datafile <- "pates_alim.csv"
+# Importation et nettoyage rapide de la serie
+datafile <- "fabricationdeboissons.csv"
 data <- read.csv(datafile,sep=";", skip=3, header = TRUE)
 
 colnames(data) <- c("Periode", "Indice","Code")
 data$Periode <- as.Date(paste0(data$Periode, "-01"))
+
 xm_source <- zoo(data$Indice, order.by = data$Periode)
 T <-length(xm_source)
 xm <- xm_source[1:(T-4)]
 
-#Tracé de la série 
-plot(xm, type = "l", xlab = "Date", ylab = "Indice")
+plot(xm, type = "l", col = "blue", xlab = "Date", ylab = "Indice")
 
-acf(coredata(xm))
-pacf(coredata(xm))
+#### QUESTION 2 : 
 
-#Première différenciation 
+# On effectue une différentiation à l'ordre 1 car on remarque une tendance dans la serie
+
 xm_diff <- diff(xm)
 
-plot(xm_diff, type = "l", xlab = "Date", ylab = "Indice")
-acf(coredata(xm_diff))
-pacf(coredata(xm_diff))
-
-
-#   QUESTION 3
+#### QUESTION 3 : 
 
 ## Ouverture d'une fenêtre à 2 graphiques
 par(mfrow = c(2, 1))  # 2 lignes, 1 colonne
@@ -44,4 +40,12 @@ plot(xm, type = "l", col = "blue", xlab = "Date", ylab = "Indice")
 # Deuxième graphique : série différenciée
 plot(xm_diff, type = "l", col = "red", xlab = "Date", ylab = "Indice différencié")
 
+#Elle semble maintenant stationnaire avec deux valeurs abérrantes liées au COVID
+
+## PARTIE 2 ##
+
+acf(coredata(xm_diff))
+pacf(coredata(xm_diff))
+
 adf.test(coredata(xm_diff), alternative = "stationary")
+pp.test(coredata(xm_diff), alternative = "stationary")
