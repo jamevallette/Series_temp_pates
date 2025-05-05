f5f6364f0e58bb7f048844a4ba3d88b6866fc580
hmorand <hugo.morand@ensae.fr>
2025-05-05 09:40:26 +0000

    on avance petit a petit

--- Code.r
+++ Code.r
@@ -4,9 +4,11 @@
 #Installation des packages necessaires
 install.packages("zoo")
 install.packages("tseries")
+install.packages("urca")
 
 require(zoo)
 require(tseries)
+library(urca)
 
 ## PARTIE 1 ##
 
@@ -43,9 +45,35 @@ plot(xm_diff, type = "l", col = "red", xlab = "Date", ylab = "Indice différenci
 #Elle semble maintenant stationnaire avec deux valeurs abérrantes liées au COVID
 
 ## PARTIE 2 ##
-
+#On affiche les acf et pacf pour estimer un ARMA(p,q) sur la serie differenciée qui semble stationnaire
 acf(coredata(xm_diff))
 pacf(coredata(xm_diff))
 
-adf.test(coredata(xm_diff), alternative = "stationary")
-pp.test(coredata(xm_diff), alternative = "stationary")
+#3 Tests pour savoir si la série admet des racines unitaires : 
+
+# ADF sans constante ni tendance
+adf_none <- ur.df(coredata(xm_diff), type = "none", selectlags = "AIC")
+summary(adf_none)
+#On rejette bien H0 qui est la présence de racine unitaire
+
+# PP sans constante ni tendance
+pp_test <- ur.pp(coredata(xm_diff), type = "Z-tau", model = "constant")
+summary(pp_test)
+#On rejette H0 la présence de racine unitaire également (avec le parametre constante qui n'est pas significatif )
+
+# Test KPSS
+test_kpss <- kpss.test(coredata(xm_diff), null = "Level")  # Série stationnaire autour de la moyenne nulle
+print(test_kpss)
+# On ne rejette pas H0 ici qui est la non présence de racine unitaire. (H0 : modèle stationnaire (TS))
+
+# On peut ainsi estimé un ARMA sur la série différenciée :
+#On affiche les acf et pacf pour estimer un ARMA(p,q) sur la serie differenciée qui semble stationnaire
+acf(coredata(xm_diff))
+pacf(coredata(xm_diff))
+
+#On estime q avec le graphe des acf => on prend q=2 (les lag après sont nuls hormis 1 ou 2 qui dépasse très peu la borne)
+#On estime p avec le graphe des pacf => on peut commencer avec p = 7
+
+#On estime le modèle ARMA sur la serie corrigée : 
+arima(coredata(xm_diff),c(7,0,2))
+
